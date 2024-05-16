07395d2464fdcb1ee5ff6b6138c0862daf56eb6a
HigorMenz <higormenzinger1997@outlook.com>
2024-05-16 14:28:29 -0400

    learn basic php

---
+++ 02-variables.php
@@ -0,0 +1,15 @@
+<?php include 'includes/header.php';
+
+//Varibales in php starts with $ sign
+$name = "Higor";
+
+echo $name;
+var_dump($name); // print more information than echo
+
+//define = const like js
+define('constant', 'cant be changed');
+echo constant; //print whitout $ sign
+
+
+
+include 'includes/footer.php';
---
+++ 03-tipos-dato.php
@@ -0,0 +1,24 @@
+<?php include 'includes/header.php';
+
+//Boolean
+$isBool = true;
+var_dump($isBool);
+
+//Int
+$number = 200;
+var_dump($number);
+
+//floats
+$float = 2.5;
+var_dump($float);
+
+//strings
+$name ="name";
+var_dump($name);
+
+$array = [];
+var_dump($array);
+
+
+
+include 'includes/footer.php';
